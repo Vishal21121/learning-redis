faf7a71a7ac4139509a35b33b677fb6a8d8c238f
Vishal21121 <vs84077000@gmail.com>
2024-02-14 20:55:39 +0530

    added: code of strings data type in redis

---
+++ strings-program/string.js
@@ -0,0 +1,13 @@
+import { Redis } from "ioredis"
+
+export const client = new Redis()
+
+async function init() {
+    await client.set("msg:2", "Hello, World!")
+    // we can also set the expiration time in seconds
+    await client.expire("msg:2", 10)
+    const result = await client.get("msg:2")
+    console.log("Result -> ", result);
+}
+
+init()
